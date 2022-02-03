f6d6d7884a7b385bd244900f46347b0e5f3deb07
Patrik <79414111+Diesel-rikky@users.noreply.github.com>
2022-02-03 11:24:38 +0100

    Add files via upload

--- akaszto/akaszto.py
+++ akaszto/akaszto.py
@@ -21,53 +21,6 @@ def cserel(feladvany, szo, betu):
     return eredmeny 
 
 
-
-def keres(feladvany, lista):
-    eredmeny=[]
-    for helyseg in lista:
-        if len(feladvany) == len(helyseg) and helyseg[0] == feladvany[0] and helyseg[-1] == feladvany[-1]:
-            eredmeny.append(helyseg)
-    return eredmeny
-
-def betutipp(feladvany:str, helyseg):
-    index = feladvany.index('.')
-    return helyseg[index]
-
-def ellenor(eredeti, uj, betu):
-    if len(eredeti) != len(uj):
-        return False
-    for i in range(len(eredeti)):
-        if eredeti[i] != uj[i]:
-            if eredeti[i] != '.':
-                return False
-            else:
-                if uj[i] != betu:
-                    return False  
-    return True
-
-def kizar(lista, betu):
-    eredmeny=[]
-    for helyseg in lista:         
-        if not betu in helyseg[1:-1]:
-            eredmeny.append(helyseg)
-    return eredmeny
-  
-def fh_gondol(helysegek):  
-    print("Gondolj egy magyar telpülésre!")
-    kiirando=input("Írd be a feladványt:")
-    lehetsegesek = keres(kiirando, helysegek)
-    #print(lehetsegesek)
-    # while ciklus inkább, amíg több mint 1 lehetséges van!
-    if len(lehetsegesek) == 0:
-        print("Nincs ilyen település.")
-    elif len(lehetsegesek) == 1:
-        print(lehetsegesek[0], "a gondolt város.")
-    else:
-        print(lehetsegesek)
-        
-        
-
-
 def main():
     helysegek = beolvas("helyek.txt")
     feladat = random.choice(helysegek)
